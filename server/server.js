8e95ef3dfcce3ea1c8b310d6a0d1b2f2087e89e8
Your Name <you@example.com>
2026-03-24 11:44:46 +0530

    some fixes

--- server/server.js
+++ server/server.js
@@ -11,13 +11,44 @@ const app=express();
 
 await connectCloudinary()
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',   // Vite
+  'http://localhost:3000',   // React / Next
+  'https://quicl-ai.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
-app.use(clerkMiddleware()) 
+app.use(clerkMiddleware());
+app.use(express.json());
+
 app.get('/',(req,res)=>{
    res.send('Server is Live')
 })
-app.use(requireAuth()) 
+//app.use(requireAuth()) 
 
 app.use('/api/ai',aiRouter)
 app.use('/api/user',userRouter)
@@ -25,6 +56,5 @@ app.use('/api/user',userRouter)
  const  PORT=process.env.PORT || 3000;
 
  app.listen(PORT,()=>{
-    console.log(`Server runnning in port ${PORT}`);
-    
+    console.log(`Server runnning in port ${PORT}`);  
  })
